1ad4196898235422dddb68375cec8df84753f795
GivaDogaBone <g_a_quintas@yahoo.com>
2020-06-09 19:13:20 +1000

    Terraform

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ resource "aws_iam_user" "circleci" {
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0077..."]
+  subnet_id              = "subnet-923a..."
 }
 
 resource "aws_iam_access_key" "circleci" {
